26b57cf966932646592c72fe98ea350098df3674
24052365-crypto <24052365@kiit.ac.in>
2025-12-10 13:30:39 +0530

    Add files via upload

---
+++ src/firstnamelastname.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class firstnamelastname{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first name: ");
+        String first = sc.nextLine();
+
+        System.out.print("Enter last name: ");
+        String last = sc.nextLine();
+
+        System.out.println(last + " " + first);
+    }
+}
+
