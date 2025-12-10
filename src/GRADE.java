1aee5d31e95b02e30cce09d275f21f3cacb97dcb
24052365-crypto <24052365@kiit.ac.in>
2025-12-10 13:00:44 +0530

    Add files via upload

---
+++ src/GRADE.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class GRADE {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the mark: ");
+        int mark = sc.nextInt();
+
+        if (mark >= 90 && mark <= 100) {
+            System.out.println("Grade: O");
+        } else if (mark >= 80) {
+            System.out.println("Grade: E");
+        } else if (mark >= 70) {
+            System.out.println("Grade: A");
+        } else if (mark >= 60) {
+            System.out.println("Grade: B");
+        } else if (mark >= 50) {
+            System.out.println("Grade: C");
+        } else if (mark >= 40) {
+            System.out.println("Grade: D");
+        } else {
+            System.out.println("Grade:F");
+        }
+
+        sc.close();
+    }
+}
