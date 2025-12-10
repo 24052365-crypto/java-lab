57a4d0a094464208145700743e0b543693b26f2b
24052365-crypto <24052365@kiit.ac.in>
2025-12-10 12:47:07 +0530

    Add files via upload

---
+++ src/student1.java
@@ -0,0 +1,7 @@
+public class student1 {
+        public static void main(String[] args) {
+            System.out.println("Name: Arsismaan Rajkhowa");
+            System.out.println("Roll No: 24052365");
+            System.out.println("Section: CSE16");
+            System.out.println("Branch: CSE");
+        }}
